1fc3fb9be76ba05d7a4d39af28319836a3d80ae7
Kalvine <kkalvine4@gmail.com>
2024-07-03 23:34:57 +0300

    Functions as Variables

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = function(arr){
+    const newDrivers = arr.slice(0, 2);
+       return newDrivers;
+    }
+// console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = function(arr){
+    const newDrivers = arr.slice(2, 4)
+    return newDrivers
+}
+// console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// console.log(selectingDrivers[1])
+
+const createFareMultiplier = function(fare){
+    function quint(value){
+        return fare*value;
+       }
+       return quint;
+    }
+// console.log(createFareMultiplier(5));
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+const selectDifferentDrivers = function(arr, foo){
+    return foo(arr)
+}
+
